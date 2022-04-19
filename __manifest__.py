d6d9eaeb0c18a1a70aac244d3e203183b4b2dc3e
linlaz <lazuardilintang@apps.ipb.ac.id>
2022-04-19 19:02:15 +0700

    dev

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Lintang Lazuardi",
     'website': "http://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
