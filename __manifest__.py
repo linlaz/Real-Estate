0c64b8ab95ef391b118138f21933f415e8c1917c
linlaz <lazuardilintang@apps.ipb.ac.id>
2022-04-19 19:21:53 +0700

    [IMP] Description

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        This Module for library
     """,
 
     'author': "Lintang Lazuardi",
